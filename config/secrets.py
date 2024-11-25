53c439843881e831430e52b4c694c1c0b25950ef
Bhanudeep Simhadri <bhanudeepsimhadry@gmail.com>
2024-11-25 17:50:59 +0530

    Create secrets.py

---
+++ config/secrets.py
@@ -0,0 +1,21 @@
+
+
+
+###################################################### CONFIGURE YOUR TOOLS HERE ######################################################
+
+
+# Login Credentials for LinkedIn (Optional)
+username = "username@example.com"  # Enter your username in the quotes
+password = "example_password"      # Enter your password in the quotes
+
+
+# # Your OPEN AI API key 
+# api_key = ""      # COMING SOON
+
+
+
+
+
+############################################################################################################
+
+############################################################################################################
